60088fba751eedd9f1996b7cd671c25c69a08bf0
Konstantin Knyazkov <constantinvk@gmail.com>
2019-03-10 23:37:04 +0100

    Add axionaut data

--- data/raw/download.cmd
+++ data/raw/download.cmd
@@ -1,4 +1,3 @@
-rem curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMA92DP/download/records.zip?pub_secret=25e23f09c8 --output records.zip
-rem unzip records.zip
-curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMCB533/download/good.zip?pub_secret=1c4f04ba90 --output good.zip
-unzip good.zip
+curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMCB533/download/good.zip?pub_secret=1c4f04ba90 --output 2018-IRONCAR-SHARED-1-250x150-JPEGS.zip
+curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMA92DP/download/records.zip?pub_secret=25e23f09c8 --output 2018-IRONCAR-SHARED-2-250x150-JPEGS.zip
+curl -L https://s3.amazonaws.com/axionautdataset/Datasets+2.zip --output 2018-AXIONAUT.zip
