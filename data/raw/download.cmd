a059556c090d2de6a0c81f45d211ab638c62fb2c
Konstantin Knyazkov <constantinvk@gmail.com>
2019-03-12 02:36:00 +0100

    Sort files

--- data/raw/download.cmd
+++
@@ -1,3 +0,0 @@
-curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMCB533/download/good.zip?pub_secret=1c4f04ba90 --output 2018-IRONCAR-SHARED-1-250x150-JPEGS.zip
-curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMA92DP/download/records.zip?pub_secret=25e23f09c8 --output 2018-IRONCAR-SHARED-2-250x150-JPEGS.zip
-curl -L https://s3.amazonaws.com/axionautdataset/Datasets+2.zip --output 2018-AXIONAUT.zip
---
+++ src/steps/etl/download.cmd
@@ -0,0 +1,13 @@
+rem curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMCB533/download/good.zip?pub_secret=1c4f04ba90 --output %1%/2018-IRONCAR-SHARED-1-250x150-JPEGS.zip
+rem mkdir %1/2018-IRONCAR-SHARED-1-250x150-JPEGS
+rem unzip %1/2018-IRONCAR-SHARED-1-250x150-JPEGS.zip -d %1/2018-IRONCAR-SHARED-1-250x150-JPEGS
+
+rem curl -L https://files.slack.com/files-pri/T2CKKMS1K-FCYMA92DP/download/records.zip?pub_secret=25e23f09c8 --output %1%/2018-IRONCAR-SHARED-2-250x150-JPEGS.zip
+rem mkdir %1/2018-IRONCAR-SHARED-2-250x150-JPEGS
+rem unzip %1/2018-IRONCAR-SHARED-2-250x150-JPEGS.zip -d %1/2018-IRONCAR-SHARED-2-250x150-JPEGS
+
+rem curl -L https://s3.amazonaws.com/axionautdataset/Datasets+2.zip --output %1%/2018-AXIONAUT.zip
+rem mkdir %1/2018-AXIONAUT.zip
+rem unzip %1/2018-AXIONAUT.zip -d %1/2018-AXIONAUT
+
+echo "Done" > %1/downloaded.txt
